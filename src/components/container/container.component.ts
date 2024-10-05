0047770659163f7092ccb4f46be6b0edd172b89d
octagony <moctagony@gmail.com>
2024-10-05 11:33:27 +0300

    [FEAT]: Added basic hex-generator component

--- src/components/container/container.component.ts
+++ src/components/container/container.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { HexGeneratorComponent } from '../hex-generator/hex-generator.component';
 
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, HexGeneratorComponent],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss',
 })
